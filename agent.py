SOFTMAX and CrossEntropyLoss FIX

agent.py
@@ -28,7 +28,7 @@ from fen_to_board import fenToBoard
 ########################### BELLMAN EQUATiON ##############################
 
 MAX_MEMORY = 100_000
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 LR = 0.001
 white = True
 black = False
@@ -41,8 +41,8 @@ class Agent:
         self.epsilon = 5  # randomness
         self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
-        self.model = Qnet(5248, 1000, 5184)
-        self.trainer = Qtrainer(self.model, lr=LR, gamma=self.gamma)
+        self.model = Qnet(5248, 500, 5184, 2)
+        self.trainer = Qtrainer(self.model, lr=0.001, gamma=self.gamma)
         self.game = None
         self.StockFish_Enginge = None
         self.agent = None
@@ -96,6 +96,7 @@ class Agent:
     def get_action(self, state, game, reward, color):
         legals = list(game.chessboard.legal_moves)
         # random moves: tradeoff exploration / exploitation
+        # EPSILON GREEDY ALGORITHM
         self.epsilon = 80 - self.n_games
         try:
             random_move = np.random.randint(0, len(legals))
@@ -144,82 +145,90 @@ def train(agent):
 
 
 def game_loop():
-    np.random.seed(7)
+    # np.random.seed(7)
     running = True
     color = white
     start = time.time()
     while running:
-        if color:
-            # get old state
-            state_old = agent.get_state(agent.game)
-            # print(state_old[0])
-            # time.sleep(5)
-            final_move_index, random_legal_move_index, reward, promotion = agent.get_action(state_old,
-                                                                                            agent.game,
-                                                                                            agent.reward, color)
-            # perform move and get new state
-            p, agent.reward = agent.game.is_valid_move(agent.game, state_old, final_move_index, random_legal_move_index,
-                                                       agent.reward)
-            # print(p)
-            if p is None:
-                agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, p, color)
-                agent.reward -= 1.
+        try:
+            if color:
+                # get old state
+                state_old = agent.get_state(agent.game)
+                # print(state_old[0])
+                # time.sleep(5)
+                final_move_index, random_legal_move_index, reward, promotion = agent.get_action(state_old,
+                                                                                                agent.game,
+                                                                                                agent.reward, color)
+                # perform move and get new state
+                p, agent.reward = agent.game.is_valid_move(agent.game, state_old, final_move_index,
+                                                           random_legal_move_index,
+                                                           agent.reward)
+                # print(p)
+                if p is None:
+                    agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, p, color)
+                    agent.reward -= 1.
+                else:
+                    agent.game.chessboard.push(p)
+                    agent.game.is_promoted(agent.game, p, promotion, color)
+                    agent.game.set_chessboard()
+                    # checks
+                    agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, p, color)
+                    # print(reward)
+                    # time.sleep(0.85)
+                    state_new = agent.get_state(agent.game)
+                    # train short memory
+                    agent.train_short_memory(state_old, final_move_index, reward, state_new, agent.done)
+
+                    # remember
+                    agent.remember(state_old, final_move_index, agent.reward, state_new, agent.done)
+                    agent.reward -= 1.
             else:
-                agent.game.chessboard.push(p)
-                agent.game.is_promoted(agent.game, p, promotion, color)
-                agent.game.set_chessboard()
-                # checks
-                agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, p, color)
-                # print(reward)
-                # time.sleep(0.85)
-                state_new = agent.get_state(agent.game)
-                # train short memory
-                agent.train_short_memory(state_old, final_move_index, reward, state_new, agent.done)
+                result = agent.StockFish_Enginge.play(agent.game.chessboard, chess.engine.Limit(time=0.010))
+                # print(result)
+                agent.game.chessboard.push(result.move)
+                agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, result.move,
+                                                                             color)
+            if color == white:
+                color = black
+            else:
+                color = white
+            if agent.done:
+                # train long memory, plot result
+                agent.n_games += 1
+                agent.train_long_memory()
 
-                # remember
-                agent.remember(state_old, final_move_index, agent.reward, state_new, agent.done)
-                agent.reward -= 1.
-        else:
-            result = agent.StockFish_Enginge.play(agent.game.chessboard, chess.engine.Limit(time=0.010))
-            # print(result)
-            agent.game.chessboard.push(result.move)
-            agent.reward, agent.done, agent.score = agent.game.play_step(agent.game, agent.reward, result.move, color)
+                if agent.score > agent.record:
+                    agent.record = agent.score
+                    agent.model.save()
 
-        if color == white:
-            color = black
-        else:
-            color = white
-        if agent.done:
-            # train long memory, plot result
-            agent.n_games += 1
-            agent.train_long_memory()
+                print('Game', agent.n_games, 'Score', agent.score, 'Record:', agent.record, 'Loss: ',
+                      agent.trainer.loss)
 
-            if agent.score > agent.record:
-                agent.record = agent.score
-                agent.model.save()
+                agent.plot_scores.append(agent.score)
+                agent.total_score += agent.score
+                agent.mean_score = agent.total_score / agent.n_games
+                agent.plot_mean_scores.append(agent.mean_score)
+                agent.plot_loss.append(agent.trainer.loss)
+                agent.plot_n_games.append(agent.n_games)
+                ##plot(agent.plot_scores, agent.mean_score, agent.plot_loss)
 
-            print('Game', agent.n_games, 'Score', agent.score, 'Record:', agent.record)
-
-            agent.plot_scores.append(agent.score)
-            agent.total_score += agent.score
-            agent.mean_score = agent.total_score / agent.n_games
-            agent.plot_mean_scores.append(agent.mean_score)
-            agent.plot_loss.append(agent.trainer.loss)
-            agent.plot_n_games.append(agent.n_games)
-            ##plot(agent.plot_scores, agent.mean_score, agent.plot_loss)
-
-            # RESET GAME
-            agent.done = False
-            running = False
-            agent.score = 0.
-            agent.reward = 0.
-            agent.game.close()
-            agent.StockFish_Enginge.quit()
-            end = time.time()
-            print("Time: ", end - start, "[ s ]")
+                # RESET GAME
+                agent.done = False
+                running = False
+                agent.score = 0.
+                agent.reward = 0.
+                agent.game.close()
+                agent.StockFish_Enginge.quit()
+                end = time.time()
+                print("Time: ", end - start, "[ s ]")
+        except Exception as e:
+            print(e)
+            agent.done = True
+            agent.reward -=100
 
 
 if __name__ == '__main__':
     agent = Agent()
     while agent.total_score <= 10000:
         train(agent)
+    print('Woow - Thats awesome !!')

model.py
@@ -4,19 +4,35 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
-
+from torch.autograd import Variable
 
 class Qnet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, num_layers):
         super().__init__()
+        self.num_of_lstm_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        # self.lstm1 = nn.LSTM(input_size, hidden_size, num_layers=self.num_of_lstm_layers, dropout=0.05,
+        #                      batch_first=True)
         self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear( hidden_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, output_size)
+        self.out = nn.Softmax(dim=0)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=1 / np.sqrt(self.input_size))
+            if module.bias is not None:
+                module.bias.data.zero_()
 
     def forward(self, x):
+        # TODO make lstm work with code
         a = F.relu(self.linear1(x))
         b = F.relu(self.linear2(a))
-        out = self.linear2(F.dropout(b, 0.2))
+        c = self.linear3(F.dropout(b, 0.1))
+        out = self.out(c)
         return out
 
     def save(self, file_name='model.pth'):
@@ -34,9 +50,10 @@ class Qtrainer:
         self.lr = lr
         self.gamma = gamma
         self.model = model
-        self.optim = optim.Adam(model.parameters(), lr=self.lr, )
-        self.criterion = nn.MSELoss()
+        self.optim = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.CrossEntropyLoss()
         self.loss = None
+        self.acc = None
 
     def train_step(self, state_old, action, reward, state_new, game_over):
         # print(state_old[0])
@@ -56,6 +73,7 @@ class Qtrainer:
             game_over = (game_over,)  # tuple with only one value
 
         # 1 : predicted Q values with current state
+
         prediction = self.model(state)
         target = prediction.clone()
         for idx in range(len(game_over)):
@@ -64,8 +82,8 @@ class Qtrainer:
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
             target[idx][torch.argmax(action).item()] = Q_new
         # 2 :Q_new =  r + gamma *max(next_predicted Q value)
-        # pred.clone()
-        # preds[argmax(actrion)] = Q_new
+        # prediction.clone()
+        # prediction[torch.argmax(action)] = Q_new
 
         self.optim.zero_grad()
         loss = self.criterion(target, prediction)